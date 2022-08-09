f490afe06fde730ec9bf6235bca25856e94e7126
Rakibul Islam <rakibul241221@gmail.com>
2022-08-09 17:35:06 +0600

    added average marks math

--- explore-math-problem.js
+++ explore-math-problem.js
@@ -1,15 +1,34 @@
 // ===============exploring some problem===================
+// node explore-math-problem.js
 
-/* 
+
+
+// ====================================01=============================
+/*
 1) Harry’s mom gave him tk 1000 and asked him to buy some oranges and
 apples. Write a program to help Harry calculate how much money the
 shopkeeper will return. The total cost of 1 kg of oranges and 1 kg of
 apples is tk 700.
  */
-const applePrice = 400;
+/* const applePrice = 400;
 const orangePrice = 300;
 const money = 1000;
 
 const totalCost = applePrice + orangePrice;
 const remaining = money - totalCost;
-console.log(remaining)
+console.log(remaining) */
+
+// ====================================02==================================
+// 2) Write a program to calculate the average marks of Mathematics,
+// Biology, Chemistry, Physics, and Bangla of a student.
+
+const mathMarks = 81;
+const biologyMarks = 75;
+const chemistryMarks = 78;
+const physicsMarks = 83;
+const banglaMarks = 74;
+const totalSubjects = 5;
+
+const totalMarks = mathMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
+const averageMarks = totalMarks / totalSubjects;
+console.log(averageMarks)
