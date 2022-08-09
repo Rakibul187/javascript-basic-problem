caabdab23441ccf7887a100c245d77c6de82d359
Rakibul Islam <rakibul241221@gmail.com>
2022-08-09 17:42:48 +0600

    added string math

--- explore-math-problem.js
+++ explore-math-problem.js
@@ -1,4 +1,4 @@
-// ===============exploring some problem===================
+// ===============exploring some problem & math===================
 // node explore-math-problem.js
 
 
@@ -10,19 +10,22 @@ apples. Write a program to help Harry calculate how much money the
 shopkeeper will return. The total cost of 1 kg of oranges and 1 kg of
 apples is tk 700.
  */
-/* const applePrice = 400;
+/*
+ const applePrice = 400;
 const orangePrice = 300;
 const money = 1000;
 
 const totalCost = applePrice + orangePrice;
 const remaining = money - totalCost;
-console.log(remaining) */
+console.log(remaining)
+*/
 
 // ====================================02==================================
 // 2) Write a program to calculate the average marks of Mathematics,
 // Biology, Chemistry, Physics, and Bangla of a student.
 
-const mathMarks = 81;
+/*
+ const mathMarks = 81;
 const biologyMarks = 75;
 const chemistryMarks = 78;
 const physicsMarks = 83;
@@ -32,3 +35,18 @@ const totalSubjects = 5;
 const totalMarks = mathMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
 const averageMarks = totalMarks / totalSubjects;
 console.log(averageMarks)
+*/
+
+// =====================03==========================
+/* 
+3) John’s teacher gave him two variables. Each variable contains a string.
+John’s teacher asked him to combine these two strings(‘I am going to
+be’ and ‘an awesome web developer’) and print them in one line. Help
+John write the program. 
+*/
+/*
+ const phrase1 = 'I am going to be';
+const phrase2 = 'an awesome web developer.';
+const sentence = phrase1 + ' ' + phrase2;
+console.log(sentence)
+ */
