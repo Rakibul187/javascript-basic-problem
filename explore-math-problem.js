e4f021754011357aadb8d5e80215b51dce08891e
Rakibul Islam <rakibul241221@gmail.com>
2022-08-09 23:36:42 +0600

    added even odd check 13 namta

--- explore-math-problem.js
+++ explore-math-problem.js
@@ -224,7 +224,9 @@ while (j <= 10) {
 
 // =========================10=======================
 
-// road crossing signal . 1. green----you can cross the road.  2. yellow ---danger, you should have to stop.   3. red ,road block you should have to wait. now write a program about traffic signal.
+// You are in a hurry to go to your school on time. But when you are crossing
+// the road, the traffic signal is red coloured. In this situation, if you try to cross the road, you may be in danger.If you notice a yellow coloured traffic signal, you should stop. If you notice a green coloured traffic signal, you should cross the road. So write a JS code, where there is a variable called signal. Its value can be green, yellow or red & we will get different activities as output depending on
+// the variable. So, hurry up.
 
 /* const signal = 'red';
 
@@ -300,6 +302,120 @@ while (i < usedPhones.length) {
     if (i === 44) {
         break
     }
-} 
+}
  */
 
+// ====================================18====================================
+//  write array using your books prices . which books are more than 200 those will skip and other book will Show .
+
+/*
+ var booksPrices = [100, 199, 130, 180, 200, 145, 267, 111];
+for (let i = 0; i < booksPrices.length; i++) {
+    let bookPrice = booksPrices[i];
+    if (bookPrice === 200) {
+        continue
+    }
+    console.log(bookPrice)
+}
+*/
+
+// =======================================19===========================
+
+/*
+ Write a function called foo() which prints “foo” and a function called bar()
+which prints “bar”. Call function bar() in the foo() function after printing. What
+will be the output? Now call the foo() to see the output.
+ */
+
+/*
+ function foo() {
+    console.log('foo')
+}
+function bar() {
+    console.log('bar')
+}
+bar()
+foo()
+*/
+
+// =============================20====================================
+
+// Write a function called make_avg() which will take an three integers and
+// return the average of those values.
+
+/*
+function average(num1, num2, num3) {
+    let average = (num1 + num2 + num3) / 3;
+    return average;
+}
+console.log(average(10, 20, 30))
+
+*/
+
+// =================================21=================================
+
+// Challenging: Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+/*
+ function mak_avg(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        let element = array[i];
+        sum = sum + element
+    }
+    let average = sum / array.length;
+    return average;
+}
+
+let array = [4, 5, 8, 9, 23];
+let result = mak_avg(array);
+console.log(result)
+*/
+
+// ==================================22================================
+/*
+Write a function called odd_even() which takes an integer value and tells
+whether this value is even or odd. You need to do it in 4 ways:
+● Has return + Has parameter
+● No return + Has parameter
+*/
+// -------------has return has parameter---------------
+/*
+function oddEven(number) {
+    if (number % 2 === 0) {
+        return 'number is even'
+    }
+    else {
+        return 'number is odd'
+    }
+}
+console.log(oddEven(5))
+ */
+
+// -------------has parameter no return--------------
+
+/*
+function oddEven(number) {
+    if (number % 2 === 0) {
+        console.log('number is even')
+    }
+    else {
+        console.log('number is odd')
+    }
+}
+oddEven(10)
+ */
+
+// ================================23============================
+// write a function which will show namta of 13 
+
+/* 
+function multification13() {
+    for (let i = 1; i <= 10; i++) {
+        let multification = 13;
+        multification = multification * i;
+        console.log(13, '*', i, '=', multification)
+    }
+}
+console.log(multification13()) 
+*/
