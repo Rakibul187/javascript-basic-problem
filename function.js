cd47b0dade1a274a8bf48d44894cc8bca0d74e0b
Rakibul Islam <rakibul241221@gmail.com>
2022-08-09 10:31:42 +0600

    added simple add multi divi

--- function.js
+++ function.js
@@ -1,4 +1,34 @@
-function greet() {
-    console.log('Hello Javascript')
+// function greet() {
+//     console.log('Hello Javascript')
+// }
+// greet()
+
+
+// function welcomeMsg(name) {
+//     console.log('Welcome mr. ', name)
+// }
+// welcomeMsg('Elon Musk')
+
+// ================simple addition===========
+// function add(num1, num2) {
+//     var sum = num1 + num2;
+//     return sum
+// }
+// const result = add(10, 20);
+// console.log(result)
+
+// ==============simple multification===========
+// function calMultiply(num1, num2) {
+//     var multification = num1 * num2;
+//     return multification
+// }
+// const result = calMultiply(10, 20);
+// console.log(result)
+
+// ==================simple divition===============
+function calDivition(num1, num2) {
+    var divition = num1 / num2;
+    return divition
 }
-greet()
+const result = calDivition(55, 5);
+console.log(result)
